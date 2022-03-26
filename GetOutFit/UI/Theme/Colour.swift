52d6da838baa6a5976e8cc3dd25f4e3f258fe45b
Bolshachkov Denis <programmerHazelTanget@gmail.com>
2022-03-26 12:01:29 +0300

    add colors theme

---
+++ GetOutFit/UI/Theme/Colour.swift
@@ -0,0 +1,19 @@
+//
+//  Colour.swift
+//  GetOutFit
+//
+//  Created by Денис Большачков on 26.03.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static var BackgroundColor: Color {
+        return Color("BackgroundColor")
+    }
+    
+    static var AccentColor: Color {
+        return Color("AccentColor")
+    }
+}
