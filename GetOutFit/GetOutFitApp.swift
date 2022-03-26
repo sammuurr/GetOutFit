dfc843651d36404df100e19fdf5b929e331d1a30
Samur Quseionov <sammuurr@gmail.com>
2022-03-26 11:46:15 +0300

    Initial Commit

---
+++ GetOutFit/GetOutFitApp.swift
@@ -0,0 +1,20 @@
+//
+//  GetOutFitApp.swift
+//  GetOutFit
+//
+//  Created by Самур Гусейнов on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct GetOutFitApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
